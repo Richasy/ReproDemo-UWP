6331b94ac1b15ecd0e20dc8a3363b3dbdab6b1df
Richasy <zar234@qq.com>
2020-07-23 11:31:13 +0800

    update

---
+++ MasterDetailSample/MasterDetailTestSample/AppViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MasterDetailTestSample
+{
+    public class AppViewModel
+    {
+        public ObservableCollection<Email> Emails = new ObservableCollection<Email>();
+
+        public AppViewModel()
+        {
+            var emails = MyEmailManager.GetEmails();
+            emails.ForEach(email => Emails.Add(email));
+        }
+    }
+}
